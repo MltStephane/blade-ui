b3badbd72448ed3054ac6e27a9e3d9e2028b9def
Stéphane MULOT <stephane@mulot.dev>
2024-04-26 18:45:32 +0200

    feat: add header component

---
+++ resources/views/components/header/index.blade.php
@@ -0,0 +1,14 @@
+@props([
+    'title' => null,
+    'actions' => null,
+])
+
+<div {{ $attributes->merge(['class' => 'border-b border-gray-200 pb-4 sm:pb-6 lg:pb-8 sm:flex sm:items-center sm:justify-between']) }}>
+    <div class="text-base font-semibold leading-6 text-gray-900">{{ $title }}</div>
+
+    @if($actions)
+        <div class="mt-3 flex sm:ml-4 sm:mt-0">
+            {!! $actions !!}
+        </div>
+    @endif
+</div>
