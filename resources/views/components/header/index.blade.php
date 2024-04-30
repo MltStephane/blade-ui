3e052e6a44dbdfcb6570000bed7079b4eb17f5d2
Stéphane MULOT <stephane@mulot.dev>
2024-04-30 20:39:24 +0200

    feat(header): improve dark text color

--- resources/views/components/header/index.blade.php
+++ resources/views/components/header/index.blade.php
@@ -4,7 +4,7 @@
 ])
 
 <div {{ $attributes->merge(['class' => 'border-b border-gray-200 dark:border-gray-800 sm:flex sm:items-center sm:justify-between']) }}>
-    <div class="text-base font-semibold leading-6 text-gray-900 dark:text-slate-200">{{ $title }}</div>
+    <div class="text-base leading-6 text-gray-900 dark:text-slate-100">{{ $title }}</div>
 
     @if($actions)
         <div class="mt-3 flex sm:ml-4 sm:mt-0 gap-x-2">
