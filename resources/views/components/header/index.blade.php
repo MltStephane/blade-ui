6f99d63e32f8f81541cbde7f462ec91e17f8d8e4
Stéphane MULOT <stephane@mulot.dev>
2024-06-15 19:31:11 +0200

    fix(header): fix header slot verification

--- resources/views/components/header/index.blade.php
+++ resources/views/components/header/index.blade.php
@@ -6,7 +6,7 @@
 <div {{ $attributes->merge(['class' => 'border-b border-gray-200 dark:border-gray-800 sm:flex sm:items-center sm:justify-between']) }}>
     <div class="text-base leading-6 text-gray-900 dark:text-slate-100">{{ $title }}</div>
 
-    @if($actions)
+    @if($actions->isNotEmpty())
         <div class="mt-3 flex sm:ml-4 sm:mt-0 gap-x-2">
             {!! $actions !!}
         </div>
