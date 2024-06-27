c66204b85ac5bdd4a533978c4fc085cbeeae7ab1
Stéphane MULOT <stephane@mulot.dev>
2024-06-27 11:24:34 +0200

    feat: improve header

--- resources/views/components/header/index.blade.php
+++ resources/views/components/header/index.blade.php
@@ -4,10 +4,10 @@
 ])
 
 <div {{ $attributes->merge(['class' => 'border-b border-gray-200 dark:border-gray-800 sm:flex sm:items-center sm:justify-between']) }}>
-    <div class="text-base leading-6 text-gray-900 dark:text-slate-100 w-full">{{ $title }}</div>
+    <div class="w-full text-base leading-6 text-gray-900 dark:text-slate-100 w-full">{{ $title }}</div>
 
     @if($actions->isNotEmpty())
-        <div class="mt-3 flex sm:ml-4 sm:mt-0 gap-x-2">
+        <div class="w-full mt-3 flex sm:ml-4 sm:mt-0 gap-x-2 justify-end">
             {!! $actions !!}
         </div>
     @endif
